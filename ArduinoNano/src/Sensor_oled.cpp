896abb3594e7f0cb7ca4a946e3e9ecc84278f5ad
matveygovorukhin <matgovor@gmail.com>
2023-04-22 00:22:00 +0300

    b*****, its work! s***, i did it!

--- ArduinoNano/src/Sensor_oled.cpp
+++ ArduinoNano/src/Sensor_oled.cpp
@@ -4,12 +4,12 @@
 Sensor_oled::Sensor_oled(){}
 
 Sensor_oled::Sensor_oled(GyverOLED<SSD1306_128x64, OLED_NO_BUFFER>* oledd, const char* humid_top, const char* temp_top, const char* poten_top){
-    Serial.begin(9600);
+    // Serial.begin(9600);
     this->oled = oledd;
     this->temp = 0;
     this->humid = 0;
     this->poten = 0;
-    Serial.print(1);
+    // Serial.print(1);
     this->humid_top = new char[strlen(humid_top) + 1];
     for (unsigned int  i=0; i <= strlen(humid_top); i++){
         this->humid_top[i] = humid_top[i];
@@ -22,82 +22,44 @@ Sensor_oled::Sensor_oled(GyverOLED<SSD1306_128x64, OLED_NO_BUFFER>* oledd, const
     for (unsigned int  i=0; i <= strlen(poten_top); i++){
         this->poten_top[i] = poten_top[i];
     }
-    Serial.print(2);
-    oled->init();        // инициализация
-    Serial.print(3);
-    oled->clear();       // очистка
-    oled->setScale(3);   // масштаб текста (1..4)
-    oled->home();        // курсор в 0,0
-    // oled->print("Привет!");
-    // // delay(1000);
+    // Serial.print('k');
+
+    // Serial.print('h');
+    // oled->clear();
+    // Serial.print(3);
     // oled->setScale(1);
-    // // курсор на начало 3 строки
-    // oled->setCursor(0, 3);
-    // oled->print("GyverOLED v1.3.2");
-    // // курсор на (20, 50)
-    // oled->setCursorXY(20, 50);
-    // float pi = 3.14;
-    // oled->print("PI = ");
-    // oled->print(pi);
-    // delay(1000);
-    
+    // Serial.print(4);
+    // oled->setCursorXY(0, 5);
+    // Serial.print(5);
+    // oled->print("Humudity: ");
+    // Serial.print(6);
+    // oled->setCursorXY(0, 26);
+    // oled->print("Tempature: ");
+    // oled->setCursorXY(0, 47);
+    // oled->print("Voltage: ");
+    // oled->update();
+    // Serial.print(4);
+}
+
+bool Sensor_oled::init(){
+
+    Serial.print('h');
     oled->clear();
+    Serial.print(3);
     oled->setScale(1);
+    Serial.print(4);
     oled->setCursorXY(0, 5);
+    Serial.print(5);
     oled->print("Humudity: ");
+    Serial.print(6);
     oled->setCursorXY(0, 26);
     oled->print("Tempature: ");
     oled->setCursorXY(0, 47);
-    oled->print("Voltage: ");
+    oled->print("Voltahuige: ");
     oled->update();
     Serial.print(4);
 }
 
-Sensor_oled::Sensor_oled(GyverOLED<SSD1306_128x64, OLED_NO_BUFFER>* oled, Interface* interf, const char* humid_top, const char* temp_top, const char* poten_top){
-    this->inte = interf;
-
-    this->oled = oled;
-    this->temp = 0;
-    this->humid = 0;
-    this->poten = 0;
-    this->humid_top = new char[strlen(humid_top) + 1];
-    for (unsigned int  i=0; i < strlen(humid_top); i++){
-        this->humid_top[i] = humid_top[i];
-    }
-    this->temp_top = new char[strlen(temp_top) + 1];
-    for (unsigned int  i=0; i < strlen(temp_top); i++){
-        this->temp_top[i] = temp_top[i];
-    }
-    this->poten_top = new char[strlen(poten_top) + 1];
-    for (unsigned int  i=0; i < strlen(poten_top); i++){
-        this->poten_top[i] = poten_top[i];
-    }
-    oled->init();        // инициализация
-    oled->clear();       // очистка
-    oled->setScale(3);   // масштаб текста (1..4)
-    oled->home();        // курсор в 0,0
-    oled->print("Привет!");
-    // delay(1000);
-    oled->setScale(1);
-    // курсор на начало 3 строки
-    oled->setCursor(0, 3);
-    oled->print("GyverOLED v1.3.2");
-    // курсор на (20, 50)
-    oled->setCursorXY(20, 50);
-    float pi = 3.14;
-    oled->print("PI = ");
-    oled->print(pi);
-    // delay(1000);
-    oled->clear();
-    oled->setScale(1);
-    oled->setCursorXY(0, 5);
-    oled->print("Humudity: ");
-    oled->setCursorXY(0, 26);
-    oled->print("Tempature: ");
-    oled->setCursorXY(0, 47);
-    oled->print("Voltage: ");
-    oled->update();
-}
 
 void Sensor_oled::setInterface(Interface* interf){
     inte = interf;
--- ArduinoNano/src/main.cpp
+++ ArduinoNano/src/main.cpp
@@ -3,19 +3,22 @@
 #include "Sensor_DHT11.h"
 #include "RF24_interface.h"
 #include "Sensor_LED.h"
-// #include "Sensor_oled.h"
+#include "Sensor_oled.h"
 // #include "Sensor_oled_two.h"
 // #include "Sensor_led_pwm.h"
 #include "Sensor_potentiometr.h"
-#include "OLED_I2C.h"
+// #include "OLED_I2C.h"
 // #include "Sensor_serva.h"
 // #include "Sensor_7disp.h"
+#include <GyverOLED.h>
 // //////////////////////////Создаем интерфейс RF24//////////////////////////////
 
 RF24 radio(9, 10); // порты D9, D10: CSN CE
 RF24* RF24Interface::Radio = &radio;
 String* RF24Interface::subscribedTopics = new String [RF24_MaxCountTopics];
 typeSensor RF24Interface::subscribs[RF24_MaxCountTopics][RF24_MaxCountSubscribers];
+
+
 
 
 // ////////////////////////Создаем сенсоры//////////////////////////////////
@@ -26,128 +29,58 @@ typeSensor RF24Interface::subscribs[RF24_MaxCountTopics][RF24_MaxCountSubscriber
 // RF24Interface rf24_inter_dht11 = RF24Interface(&dht11);
 
 // //создаем LED
-Sensor_LED LED = Sensor_LED(7);
+Sensor_LED LED = Sensor_LED(5);
 RF24Interface LED_inter = RF24Interface(&LED);
 
 // создаем led pwm
 // Sensor_led_pwm led_pwm = Sensor_led_pwm(5);
 // RF24Interface led_pwm_inter = RF24Interface(&led_pwm);
 
-// // создаем oled
-// // GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oleddd;
-// // Sensor_oled oled = Sensor_oled(&oleddd, "humb", "temp", "resist");
-// // RF24Interface oled_inter = RF24Interface(&oled);
+// создаем oled
+GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+Sensor_oled oleddd = Sensor_oled(&oled, "humb", "temp", "resist");
+RF24Interface oled_inter = RF24Interface(&oleddd);
 
-// // создаем oled_two
-// Sensor_oled_two oled_two = Sensor_oled_two("humb", "temp", "resist");
-// RF24Interface oled_inter_two = RF24Interface(&oled_two);
 
 // OLED myOLED = OLED(18, 19, 8);
-OLED myOLED(SDA, SCL, 8); 
+// OLED myOLED(SDA, SCL, 8); 
 
 // ///////////////////////Основной код//////////////////////////////////////
 
-// void setup() {
-//   Serial.begin(9600);
-//   Serial.print(startRf24(&radio));
-//   oled.begin();
-//   oled.setFont(TinyFont);
-//   oled.print("aaaaaaaaaaa", CENTER, 10);
-//   oled.update();
-//   // oled_two.begin();
-//   // LED.setInterface(&LED_inter);
-//   // LED_inter.subscribe("LED_nano");
+// // подключаем шрифты для текста и цифр
+// extern uint8_t TinyFont[];
+// extern uint8_t SmallFont[];
+// extern uint8_t MediumNumbers[];
+// extern uint8_t BigNumbers[];
 
-//   // dht11.setInterface(&rf24_inter_dht11);
-
-//   // oled_two.setInterface(&oled_inter_two);
-//   // oled_inter_two.subscribe("hump");
-//   // oled_inter_two.subscribe("temp");
-//   // oled_inter_two.subscribe("resist");
-// }
-
-
-          // подключаем библиотеку для экрана  // создаем объект myOLED
-
-// подключаем шрифты для текста и цифр
-extern uint8_t TinyFont[];
-extern uint8_t SmallFont[];
-extern uint8_t MediumNumbers[];
-extern uint8_t BigNumbers[];
-
-uint64_t timer1;
+// uint64_t timer1;
 
 void setup(){
   Serial.begin(9600);
   Serial.print(startRf24(&radio));
-  myOLED.begin();  // инициализируем экран
+
+  oleddd.setInterface(&oled_inter);
+  oleddd.init();
+  // oled.clear();
+  // oled.setScale(1);
+  // oled.setCursorXY(0, 5);
+  // oled.print("Humudity: ");
+  // oled.setCursorXY(0, 26);
+  // oled.print("Tempature: ");
+  // oled.setCursorXY(0, 47);
+  // oled.print("Volthuiage: ");
+  // oled.update();
+  // Serial.print(4);
 
   LED.setInterface(&LED_inter);
   LED_inter.subscribe("LED_nano");
 
   // dht11.setInterface(&rf24_inter_dht11);
-  timer1 = millis();
-  
-
-  // led_pwm.setInterface(&led_pwm_inter);
-
-  // oled_two.setInterface(&oled_inter_two);
-  // oled_inter_two.subscribe("hump");
-  // oled_inter_two.subscribe("temp");
-  // oled_inter_two.subscribe("resist");
-  //    myOLED.setFont(TinyFont);
-  //  myOLED.print("ABCDEFGHIJKLMNOPQRSTUVWXYZ", LEFT, 10);
-  //  myOLED.update();
- 
-  //  // выводим текст по центру экрана
-
-  // myOLED.setFont(TinyFont);
-  // myOLED.print("ABCDEFGHIJKLMNOPQRSTUVWXYZ", LEFT, 10);
-  // myOLED.update();
+  // timer1 = millis();
  
 }
 
 void loop(){
-  // Serial.println(dht11.iteration());
-  // dht11.iteration();
-  // 
   LED.iteration();
-  // led_pwm.iteration();
-  // oled.iteration();
-  // oled_two.iteration();   
-
-  // выводим текст по левому краю экрана
-
-  if(millis() - timer1 > 5000){
-    // myOLED.setFont(TinyFont);
-    // myOLED.print("ABCDEFGHIJKLMNOPQRSTUVWXYZ", LEFT, 10);
-    // myOLED.update();
-    Serial.println("t");
-    timer1 = millis();
-  
-
-  // // выводим текст по центру экрана
-  myOLED.setFont(SmallFont);
-  myOLED.print("ABCDEFG", CENTER, 25);
-  myOLED.update();
-
-  myOLED.clrScr(); // очищаем экран от надписей
-  myOLED.update();
-  myOLED.invert(true); // включаем инверсию экрана
-
-  // выводим цифры по правому краю экрана
-  myOLED.setFont(MediumNumbers);
-  myOLED.print("123456789", RIGHT, 5);
-  myOLED.update();
-
-  // выводим цифры, начиная с 5 пикселя строки
-  // myOLED.setFont(BigNumbers);
-  // myOLED.print("123456789", 5, 30);
-  // myOLED.update();
-
-  // myOLED.clrScr(); // очищаем экран от надписей
-  // myOLED.update();
-  // myOLED.invert(false); // отключаем инверсию экрана
-  }
-
+  oleddd.iteration();
 }
